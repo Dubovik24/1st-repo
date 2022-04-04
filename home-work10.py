1e1da788cb58bfed0e00b2f410a5a328ae9704de
Olga Dubovik <olga.dubovik.86@gmail.com>
2022-04-04 15:15:00 +0300

    create a new repository

---
+++ home-work10.py
@@ -0,0 +1,12 @@
+# 1.
+
+a = ('yellow', 'red', 'white', 'green')
+print(max(a))
+
+# 2.
+
+a = "Italy, Greece, Spain, England, Netherlands"
+a = a.replace(',', ' ')
+print(a)
+b = a.split()
+print(type(b), b)
